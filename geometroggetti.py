c59a4aa646c9e4caf573bd0e58283711d3a05782
emanbet <51517811+emanbet@users.noreply.github.com>
2019-06-11 16:03:44 +0200

    Update geometroggetti.py

--- geometroggetti.py
+++ geometroggetti.py
@@ -1,77 +1,41 @@
-# Questo è il miglior programma di geometria piana del mondo ed e' oggettivamente ad oggetti
-from math import sqrt
-
 class Quadrato:
-    areaQuad = 0
-
-    def __init__(self, ilLato):
-        self.lato = ilLato
-        self.areaQuad = ilLato * ilLato
-
-    def area(self):
-        return self.areaQuad
-
-    def diagonale(self):
-        return self.lato * sqrt(2)
-
-class QuadratoPlus:
-    __areaQuad = "Non hai definito il lato"
-    __diagonaleQuad = "Senza lato ti attacchi"
-    __lato = 0
-
-    def inizializza(self, ilLato):
-        self.__lato = ilLato
-        self.__areaQuad = ilLato * ilLato
-        self.__diagonaleQuad = sqrt(2) * ilLato
-
-    def area(self):
-        return self.__areaQuad
-
-    def diagonale(self):
-        return self.__diagonaleQuad
-
-    def getLato(self):
-        return self.__lato
+    def __init__(self, base):
+        self.base = base
+        self.area = base * base
+        self.perimetro = 4 * base
+        self.diagonale = sqrt(2) * base
 
 class Triangolo:
     def __init__(self, base, altezza):
         self.base = base
         self.altezza = altezza
+        self.area = base*altezza/2
 
-    def area(self):
-        return self.base * self.altezza / 2
+def verifica(num):
+        while num<=0:
+            print("questo numero non può essere minore o uguale a 0 inserisci un nuovo valore")
+            num = int(input())
+        return num
 
 def menu():
     print("\nDi che figura vuoi calcolare l'area? \n 1. Quadrato \n 2. Triangolo \n 3. Esci dal programma \n")
     figura = input("Inserisci il numero corrispondente alla tua scelta ")
-
     if figura == "1":
         temp = int(input("Dimmi la lunghezza del lato "))
-        q1 = Quadrato(temp)
-        q2 = QuadratoPlus()
-        print (q2.area())
-        q2.inizializza(temp)
-        print ("Area di Q2", q2.area())
-        print("Area di Q1" ,q1.area())
-
-        print(q2.getLato())
-        q2.__lato = 24
-        print(q2.area())
-        print(q2.getLato())
+        q1 = Quadrato(verifica(temp))
+        print("Quadrato")
+        print("Lato : {} \n area : {} \n  perimetro : {}".format(q1.base,q1.area,q1.perimetro))
         ancora()
-
     elif figura == "2":
-        t1 = Triangolo((int(input("Dimmi la lunghezza della base "))), (int(input("Dimmi la lunghezza dell'altezza "))))
-        print("Sia noto a tutti che l'area del tuo triangolo è pari a ", t1.area())
+        t1 = Triangolo(verifica(int(input("Dimmi la lunghezza della base "))), verifica(int(input("Dimmi la lunghezza dell'altezza "))))
+        print("Sia noto a tutti che l'area del tuo triangolo è pari a ", t1.area)
         ancora()
-
     elif figura == "3":
         print("\n \n Ad maiora! \n \n ")
         quit()
-
     else:
         errore_num()
-        ancora()
+        menu()
 
 def errore_num():
     print("Guarda che non e' difficile, basta premere 1, 2 oppure 3")
@@ -93,4 +57,5 @@ def ancora():
     else:
         errore_sn()
         ancora()
+
 menu()
